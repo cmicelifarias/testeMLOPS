6540a3d5765a38f82c4ebb779e60c97a854e646a
cmicelifarias <cmicelifarias@users.noreply.github.com>
2022-10-20 21:09:00 -0300

    Update modelo.py

--- modelo.py
+++ modelo.py
@@ -8,6 +8,7 @@ modelo = tf.keras.Sequential([minha_camada])
 modelo.compile(optimizer="sgd", loss="mean_squared_error")
 
 # y = 2x+1
+# meu pseudo BD - nao da para ficar trocando manualmente o tempo todo
 xs = np.array([0,  1, 2, 3, 4], dtype=float)
 ys = np.array([1, 3, 5, 7, 9], dtype=float)
 
